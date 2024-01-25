4498c1eed84d064da6831a42ecf046e7fee01a5d
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-01-25 13:37:56 +0000

    Built site for gh-pages

--- www/material/2016-09/script.R
+++ www/material/2016-09/script.R
@@ -7,26 +7,26 @@ plot(t, s, type="s", xlab="Time", ylab="State")
 
 
 calc_time_in_states <- function(times, states, num_states) {
-  
+
   # check if times same length as states, throw exception if not
   if(length(times) != length(states)) stop('Time and state vectors not of equal length')
 
   # check that times is strictly increasing vector
   if(!identical(times, sort(times))) stop('Time vector is not strictly increasing')
-    
+
   # check there is only valid states in the state vector
   if(any(!(states %in% 1:num_states))) stop('State vector includes invalid state entries')
-    
+
   # calc time in each state using a for loop
   time_in_state <- rep(0, times = num_states)
   for(i in 2:length(times)) {
-      time_in_state[states[i-1]] <- time_in_state[states[i-1]] + times[i] - times[i-1] 
+      time_in_state[states[i-1]] <- time_in_state[states[i-1]] + times[i] - times[i-1]
   }
 
   return(time_in_state)
 }
 
-checkEquals(calc_time_in_states(times = t, states = s, num_states = n), 
+checkEquals(calc_time_in_states(times = t, states = s, num_states = n),
             c(29, 47, 24))
 
 
@@ -37,22 +37,22 @@ invalid_t <- c(0, 17,32,27,42,46,53,62,75,85,86,92, 100)
 checkException(calc_time_in_states(times = invalid_t, states = s, num_states = n))
 
 bugged_calc_time_in_states <- function(times, states, num_states) {
-    
+
     # check if times same length as states, throw exception if not
     if(length(times) != length(states)) stop('Time and state vectors not of equal length')
-    
+
     # check that times is strictly increasing vector
     if(!identical(times, sort(times))) stop('Time vector is not strictly increasing')
-    
+
     # check there is only valid states in the state vector
     if(any(!(states %in% 1:num_states))) stop('State vector includes invalid state entries')
-    
+
     # calc time in each state using a for loop
     time_in_state <- rep(0, times = num_states)
     for(i in 2:length(times)) {
         time_in_state[states[i-1]] <- time_in_state[states[i-1]] + times[i] - times[i-1] + 1
     }
-    
+
     return(time_in_state)
 }
 
@@ -63,7 +63,7 @@ checkEquals(bugged_calc_time_in_states(times = t, states = s, num_states = n),
 
 
 #######
-SimContMarkov <- function(T, s_0, G) 
+SimContMarkov <- function(T, s_0, G)
 {
     t <- 0; s <- s_0; states <- c(); times <- c()
     while(t <= T)
@@ -73,7 +73,7 @@ SimContMarkov <- function(T, s_0, G)
         sStar <- sample((1:nrow(G))[-s], 1, prob=G[s,-s]/-G[s,s])
         t <- t + tStar; s <- sStar
     }
-    return(list(states=states, times=times))    
+    return(list(states=states, times=times))
 }
 
 simTime <- 100
